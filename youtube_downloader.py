42c1553f92b3b39d6faf0e18d8ecf43d4ea58011
nikolay_dimitrov256 <raystlin7@gmail.com>
2024-08-12 22:45:57 +0300

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,22 +1,49 @@
+import json
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 from typing import Optional
 
 from loaders import BookmarkLoader
 
 
 class YoutubeDownloader:
+    supported_browsers = ['Mozilla Firefox', 'Google Chrome']
+
     def __init__(self):
+        self.settings = self.load_settings()
         self.loader: Optional[BookmarkLoader] = None
         self.root = tk.Tk()
         self.root.title('Youtube Downloader')
 
         self.render_widgets()
 
-    def render_widgets(self):
-        main_frame = ttk.Frame(self.root)
-        main_frame.pack()
+    @staticmethod
+    def load_settings():
+        try:
+            with open('settings.txt') as file:
+                settings = json.loads(file.read())
 
+                return settings
+        except FileNotFoundError:
+            return {}
+
+    def save_settings(self):
+        with open('settings.txt', 'w') as file:
+            file.write(json.dumps(self.settings))
+
+    def render_widgets(self):
+        main_tab = ttk.Notebook(self.root)
+        main_frame = ttk.Frame(main_tab)
+        settings_frame = ttk.Frame(main_tab)
+        main_tab.add(main_frame, text='Downloader')
+        main_tab.add(settings_frame, text='Settings')
+        main_tab.pack()
+
+        self.render_download_tab(main_frame)
+
+        self.render_settings_tab(settings_frame)
+
+    def render_download_tab(self, main_frame: ttk.Frame):
         download_frame = ttk.LabelFrame(main_frame, text='Download from single URL')
         download_frame.grid(row=0, column=0)
 
@@ -46,6 +73,48 @@ class YoutubeDownloader:
         message_field = tk.Text(message_frame, width=50, height=20, yscrollcommand=message_scroll.set)
         message_field.pack()
         message_scroll.config(command=message_field.yview)
+
+    def render_settings_tab(self, settings_frame: ttk.Frame):
+        # message_entry = ttk.Entry(settings_frame, state=tk.DISABLED)
+        # message_entry.grid(row=0, column=0, padx=5, pady=5, sticky='w')
+        #self.output_entry_message(message_entry, 'Chose a browser', 'green')
+
+        ttk.Label(settings_frame, text='Chose a browser: ').grid(row=2, column=0, padx=5, pady=5, sticky='w')
+        current_browser = tk.StringVar()
+        select_browser_dropdown = ttk.Combobox(
+            settings_frame,
+            textvariable=current_browser,
+            width=30,
+            values=self.supported_browsers
+        )
+        select_browser_dropdown.grid(row=2, column=1, pady=5, sticky='w')
+
+        ttk.Button(
+            settings_frame,
+            text='Select Path',
+            command=lambda: self.select_path_to_bookmarks(current_browser.get()),
+        ).grid(row=4, column=1, sticky='w')
+
+        ttk.Button(
+            settings_frame,
+            text='Save',
+            command=self.save_settings
+        ).grid(row=6, column=1, pady=30)
+
+    def select_path_to_bookmarks(self, browser: str):
+        if browser not in self.supported_browsers:
+            return
+        
+        path = filedialog.askdirectory()
+        self.settings[browser] = path
+
+    def output_entry_message(self, entry: ttk.Entry, message: str, color: str = 'black'):
+        entry['state'] = tk.NORMAL
+        entry.delete(0, tk.END)
+        entry.insert(0, message)
+        entry['foreground'] = color
+        entry['state'] = tk.DISABLED
+
 
     def download_video(self, url):
         print(url)
